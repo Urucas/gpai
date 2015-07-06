da82233649f30e07c1092f3a5b91e284ed7b86a0
vrunoa <vruno.alassia@gmail.com>
2015-07-06 11:49:32 -0300

    update service

--- service/api.js
+++ service/api.js
@@ -2,22 +2,15 @@ import express from 'express';
 import gapi from '../lib';
 
 let app = express();
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header('Access-Control-Allow-Credentials', true);
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
-
 
 app.get("/*", (req, res) => {
   let id = req.query.id
   if(id == undefined) { 
-    res.jsonp(req.query.callback + '('+ JSON.stringify({error:"Package id not defined"}) + ');');
+    res.jsonp({error:"Package id not defined"});
     return;
   }
   gapi({id:id}).then( (info) => {
-    res.jsonp(req.query.callback + '('+ JSON.stringify(info) + ');');
+    res.jsonp(info);
   });
 });
 
