9311c2899542e504bebc77d75c6fa188e37b6e07
vrunoa <vruno.alassia@gmail.com>
2015-07-06 11:41:20 -0300

    update service

--- service/api.js
+++ service/api.js
@@ -5,6 +5,7 @@ let app = express();
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Credentials', false);
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
