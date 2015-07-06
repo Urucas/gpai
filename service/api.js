6860cdc1f4c4e4122082749e3143deab172a0637
vrunoa <vruno.alassia@gmail.com>
2015-07-06 01:51:03 -0300

    add gapi service

---
+++ service/api.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import gapi from '../lib';
+
+let app = express();
+
+app.get("/*", (req, res) => {
+  let pkg = req.query.id
+  if(pkg == undefined) res.json({error:"Package id not defined"});
+  gapi({_package_:pkg}).then( (info) => {
+    res.json(info);
+  });
+});
+
+let server = app.listen(process.env.port || 5000, (err) => {
+  console.log("Service running");
+  if(err) {
+    console.log(err);
+    process.exit(1);
+  }
+});
+
